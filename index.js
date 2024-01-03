0f7c9a4ca07defbd89ed0cbc1341759441dfdd2e
Chetan <chetanmalviya.182@gmail.com>
2024-01-03 21:11:09 +0530

    COMPLETED

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const blogs = [];
-
-// app.use(express.static('public'))
-
+let blogs = [];
+let nextBlogId = 1;
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
@@ -24,16 +22,56 @@ app.post("/new_post", (req, res) =>{
     var postContent = req.body['postContent'];
 
     const postObj = {
-        "title":postTile,
-        "content":postContent
+        id: nextBlogId++,
+        title: postTile,
+        content: postContent
     }
     blogs.push(postObj);
-    res.render("view_post.ejs", {todaysBlog: blogs});
-})
+    res.render("view_post.ejs", {todaysBlog: blogs}); 
+});
 
 app.get("/view_post", (req, res) =>{
     res.render("view_post.ejs", {todaysBlog: blogs});
-})
+});
+
+app.get("/update_post/:id", (req, res) =>{
+    const id = parseInt(req.params.id);
+    const blogToUpdate = blogs.find(blog => blog.id === id);
+
+    if (blogToUpdate) {
+        res.render("update_post.ejs", { blog: blogToUpdate });
+    } else {
+        res.status(404).send("Blog post not found");
+    }
+});
+
+app.post("/update_post/:id", (req, res) =>{
+    const id = parseInt(req.params.id);
+    const blogToUpdate = blogs.find(blog => blog.id === id);
+
+    if (blogToUpdate) {
+        blogToUpdate.title = req.body['postTitle'];
+        blogToUpdate.content = req.body['postContent'];
+        res.redirect("/view_post");
+    } else {
+        res.status(404).send("Blog post not found");
+    }
+});
+
+app.post("/delete_post/:id", (req, res) =>{
+    const id = parseInt(req.params.id);
+
+    // Find the index of the blog post with the given id
+    const index = blogs.findIndex(blog => blog.id === id);
+
+    if (index !== -1) {
+        const deletedPost = blogs.splice(index, 1);
+        res.json({ success: true, message: "Post deleted successfully" });
+    } else {
+        res.status(404).json({ success: false, message: "Blog post not found" });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
